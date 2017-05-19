a28df01f1da687c16341044419e724ba2fc0f534
Ryan Stellar <rstellar@gmail.com>
2017-05-19 12:39:15 -0400

    debugginb build- reorder webpack presets in config

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     loaders: [
       {
         query: {
-          presets: ['es2015', 'react', 'stage-1'],
+          presets: ['react', 'es2015', 'stage-1'],
         },
         test: /\.js$/,
         loader: [
